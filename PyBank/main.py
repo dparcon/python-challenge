974849de33b8f257ddc4af6746bc0492da28179c
danterparcon@gmail.com <danterparcon@gmail.com>
2024-05-22 14:09:46 -0700

    added additional comments for clarity

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 import os, csv
 from pathlib import Path 
 
+#Set the path to access budget_data.csv
 budget_data_path = Path("PyBank/Resources/budget_data.csv")
 
 
@@ -11,32 +12,43 @@ previous_profit_change = 0
 average_change = 0
 greatest_increase = 0
 greatest_decrease = 0
-
 profit_changes = []
 
+#Read budget_data.csv for analysis
 with open(budget_data_path, newline="", encoding="utf-8") as file:
     csvreader = csv.reader(file, delimiter=",")
+    
     # Skip the header row
     next(csvreader) 
     budget_data = list(csvreader)
 
-    # Loop through each row in the CSV file
+# Loop through each row in the CSV file
 for row in budget_data:
     total_months += 1
     net_total += int(row[1])
+    
+    #Calculate and record changes in profit and loss
     if previous_profit_change != 0:
         change = int(row[1]) - previous_profit_change
         profit_changes.append(change)
+        
+        #Track greatest increase and date
         if change > greatest_increase:
             greatest_increase = change
             greatest_increase_date = row[0]
+        
+        #Track greatest decrease and date
         if change < greatest_decrease:
             greatest_decrease = change
             greatest_decrease_date = row[0]
+    
+    #Set new previous change for consideration        
     previous_profit_change = int(row[1])
 
+#Calculate average change
 average_change = round((sum(profit_changes) / len(profit_changes)), 2)
 
+#Print financial analysis of budget_data.csv
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
@@ -45,10 +57,11 @@ print(f"Average Change: ${average_change}")
 print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
 print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
 
+#Write path for new text file
 output_file = Path("PyBank", "analysis", "pybank_analysis.txt")
 
-with open(output_file, "w") as file:
-    
+#Write analysis to text file
+with open(output_file, "w") as file:   
     file.write("Financial Analysis")
     file.write("\n")
     file.write("----------------------------")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,10 @@
 import os, csv
 from pathlib import Path 
 
+#Set the path to access election_data.csv
 election_data_path = Path("PyPoll/Resources/election_data.csv")
-folder_name = "analysis"
-file_name = "pypoll_analysis.txt"
-file_path = Path("PyPoll/analysis")
 
-#notes
+# Initialize variables to store financial data
 total_votes = 0
 Stockham_total = 0
 DeGette_total = 0
@@ -15,15 +13,16 @@ Stockham_percent = 0
 DeGette_percent = 0
 Doane_percent = 0
 winner = 0
-
 candidates = []
 
+#Read election_data.csv for analysis
 with open(election_data_path, newline="", encoding="utf-8") as file:
     csvreader = csv.reader(file, delimiter=",")
     # Skip the header row
     next(csvreader) 
     election_data = list(csvreader)
-    
+
+# Loop through each row in the CSV file addin oneg vote to each candidates total when they receive a vote    
 for row in election_data:
     total_votes += 1
     if row[2] == "Charles Casper Stockham":
@@ -33,17 +32,20 @@ for row in election_data:
     if row[2] == "Raymon Anthony Doane":
         Doane_total += 1
 
+#Calculate percentages of each candidates total votes
 Stockham_percent = round((Stockham_total / total_votes)*100,3)
 DeGette_percent = round((DeGette_total / total_votes)*100, 3)
 Doane_percent = round((Doane_total / total_votes)*100, 3)
 
+#Determine the winner of the election by comparing vote totals
 if Stockham_total > DeGette_total and Stockham_total > Doane_total:
     winner = "Charles Casper Stockham"
 if DeGette_total > Stockham_total and DeGette_total > Doane_total:
     winner = "Diana DeGette"
 if Doane_total > Stockham_total  and Doane_total > DeGette_total:
     winner = "Raymon Anthony Doane"
-    
+
+#Print financial analysis of election_data.csv  
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
@@ -55,8 +57,10 @@ print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
 
+#Write path for new text file
 output_file = Path("PyPoll", "analysis", "pypoll_analysis.txt")
 
+#Write analysis to text file
 with open(output_file, "w") as file:
     file.write("Election Results")
     file.write("\n")
